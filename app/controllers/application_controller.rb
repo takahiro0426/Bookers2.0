45377e9cb1423b90374a3a7c4b2d6ba6177ae428
vagrant <vagrant@localhost.localdomain>
2020-08-18 14:25:30 +0000

    [Add] Bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
 	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	def after_sign_in_path_for(resource)
+		flash[:notice] = "Signed in successfully."
+		user_path(resource.id)
+	end
+
+	def after_sign_out_path_for(resource)
+		home_top_path
+	end
+
+
+
 
 	protected
 	def configure_permitted_parameters
